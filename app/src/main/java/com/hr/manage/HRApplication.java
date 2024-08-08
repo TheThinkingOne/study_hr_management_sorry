e4c26395f84447c2cf2e3bc77ed6cdd28010dd53
frod <won1680@naver.com>
2024-08-08 16:42:36 +0900

    Feat: implement application class

--- app/src/main/java/com/hr/manage/HRApplication.java
+++ app/src/main/java/com/hr/manage/HRApplication.java
@@ -1,4 +1,10 @@
 package com.hr.manage;
 
+import com.hr.manage.controller.ManageController;
+
 public class HRApplication {
+	public static void main(String[] args) {
+		ManageController manageController = new ManageController();
+		manageController.run();
+	}
 }
