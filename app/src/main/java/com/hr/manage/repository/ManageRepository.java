4f6aa68ee4d94c3cee299916b9265b126b1f3b46
frod <won1680@naver.com>
2024-08-08 17:34:01 +0900

    Chore: implement repository structure

--- app/src/main/java/com/hr/manage/repository/ManageRepository.java
+++ app/src/main/java/com/hr/manage/repository/ManageRepository.java
@@ -16,23 +16,49 @@ public class ManageRepository {
 	}
 
 	// responseDTO return 으로 변경
-	public String find() {
+	public String findDepartmentWorkAttendance(int id){
 
 		String query = "";
 
+/*
 		try {
 			ResultSet resultSet = stmt.executeQuery(query);
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+*/
+
+		System.out.println("멤버 근태 조회 로직");
 
 		return "find data";
 	}
 
-	public String insert(WorkAttendanceRequestDTO workAttendanceRequestDTO){
+
+	// responseDTO return 으로 변경
+	public String findMemberWorkAttendance(int id){
 
 		String query = "";
 
+/*
+		try {
+			ResultSet resultSet = stmt.executeQuery(query);
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+*/
+
+		System.out.println("팀 근태 조회 로직");
+
+		return "find data";
+	}
+
+
+	public String insert(WorkAttendanceRequestDTO workAttendanceRequestDTO){
+
+		String query = "";
+/*
 		try {
 
 			stmt.executeUpdate(query);
@@ -40,6 +66,9 @@ public class ManageRepository {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+	*/
+
+		System.out.println("입력 로직");
 
 		return "근태 정보가 입력되었습니다.";
 	}
@@ -49,6 +78,7 @@ public class ManageRepository {
 
 		String query = "";
 
+/*
 		try {
 
 			stmt.executeUpdate(query);
@@ -56,21 +86,25 @@ public class ManageRepository {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+*/
 
 		return "근태 정보가 수정되었습니다.";
 	}
 
-	public String delete(long id) {
+	public String delete(long id, String date) {
 
 		String query = "";
 
+/*
 		try {
 
-			stmt.executeUpdate(query);
+			// stmt.executeUpdate(query);
+			System.out.println("삭제 로직");
 
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+*/
 
 		return "근태 정보가 삭제되었습니다.";
 	}
