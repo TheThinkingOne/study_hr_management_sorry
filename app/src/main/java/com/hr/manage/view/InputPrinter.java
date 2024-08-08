2fe92f71e0ebd2675f300e00e325691ef8fe7f8c
frod <won1680@naver.com>
2024-08-08 15:42:32 +0900

    Feat: Implement input function

---
+++ app/src/main/java/com/hr/manage/view/InputPrinter.java
@@ -0,0 +1,31 @@
+package com.hr.manage.view;
+
+import java.util.Scanner;
+
+public class InputPrinter {
+
+	public static String CHOOSE_MESSAGE = "선택하세요 : ";
+	public static String USER_ID_MESSAGE = "직원 ID 입력: [EX: 12345] ";
+	public static String DATE_MESSAGE = "날짜 입력 (YYYY-MM-DD): [EX: 2024-08-01] ";
+	public static String WORK_ATTENDANCE_MESSAGE = "근무 상태 입력 (출근/퇴근/휴가 등): [EX: 출근] ";
+
+
+	Scanner scanner;
+
+	public InputPrinter() {
+		this.scanner = new Scanner(System.in);
+	}
+
+	public String inputString(String message) {
+		System.out.print(message);
+		return scanner.nextLine();
+	}
+
+	public int inputInt(String message) {
+		System.out.print(message);
+		return scanner.nextInt();
+	}
+
+
+
+}
